9c943fe90b2a7df6b7b380cc1e12d1b4f5a785a8
藍奕濡 <a78945612385238@gmail.com>
2020-08-20 15:37:16 +0800

    add coupon effective_date

--- router/user/coupon.js
+++ router/user/coupon.js
@@ -11,8 +11,10 @@ router.post('/', async (req, res) => {
     const coupon = Object.values(coupons).find((item) => item.code === code);
     if (!coupon) return res.send({ success: false, message: '找不到優惠卷' });
     if (!coupon.is_enabled) return res.send({ success: false, message: '優惠卷未啟用' });
+    if (coupon.effective_date > Date.now()) return res.send({ success: false, message: '優惠卷尚未生效' });
     if (coupon.due_date < Date.now()) return res.send({ success: false, message: '優惠卷已過期' });
     const cartSnapshot = await db.ref('/carts').child(uid).once('value');
+    if (!cartSnapshot.exists()) return res.send({ success: false, message: '禁止購物車為空' });
     const cart = cartSnapshot.val();
     const updateCart = Object.entries(cart).reduce((arr, cartProduct) => {
       const newArr = arr;
