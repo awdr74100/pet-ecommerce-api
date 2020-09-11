8fced829dde05eb7a3192c3576980e53ef926235
藍奕濡 <a78945612385238@gmail.com>
2020-09-12 03:16:04 +0800

    bug fix

--- router/admin/coupons.js
+++ router/admin/coupons.js
@@ -6,7 +6,7 @@ router.post('/', async (req, res) => {
   const data = req.body;
   const coupon = {
     title: data.title || '',
-    is_enabled: data.is_enabled === undefined ? true : data.is_enabled,
+    is_enabled: data.is_enabled === undefined ? false : data.is_enabled,
     percent: data.percent || 100,
     effective_date: data.effective_date || Date.now(), // 預設當下開始生效
     due_date: data.due_date || Date.now() + 86400000 * 7, // 預設 7 天到期
